04284ad874af66bb4e46da88dba4700ce6bd0166
gerald <geraldmontufar@gmail.com>
2025-04-23 10:05:04 +0200

    added annotations to model

--- src/main/java/com/geraldsaccount/neuefische_todo/model/Task.java
+++ src/main/java/com/geraldsaccount/neuefische_todo/model/Task.java
@@ -1,5 +1,11 @@
 package com.geraldsaccount.neuefische_todo.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.With;
+
+@With
+@Document("tasks")
 public record Task(String id, String description, TaskStatus status) {
 
 }
