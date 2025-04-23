7901b2ca8da13b078215fccc84175d0b95f3ba2d
gerald <geraldmontufar@gmail.com>
2025-04-23 09:47:27 +0200

    initial commit

---
+++ src/main/java/com/geraldsaccount/neuefische_todo/NeuefischeTodoApplication.java
@@ -0,0 +1,13 @@
+package com.geraldsaccount.neuefische_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuefischeTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuefischeTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geraldsaccount/neuefische_todo/NeuefischeTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geraldsaccount.neuefische_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuefischeTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
