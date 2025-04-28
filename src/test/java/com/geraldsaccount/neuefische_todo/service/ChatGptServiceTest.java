c1d6374dc8c4a3848d62d792d2202e14c890803c
gerald <geraldmontufar@gmail.com>
2025-04-28 15:43:51 +0200

    added string correction using chatgpt

--- src/test/java/com/geraldsaccount/neuefische_todo/service/ChatGptServiceTest.java
+++ src/test/java/com/geraldsaccount/neuefische_todo/service/ChatGptServiceTest.java
@@ -1,11 +1,8 @@
 package com.geraldsaccount.neuefische_todo.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,16 +12,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.geraldsaccount.neuefische_todo.model.openai.OpenAiContent;
+import com.geraldsaccount.neuefische_todo.model.openai.OpenAiException;
 import com.geraldsaccount.neuefische_todo.model.openai.OpenAiOutput;
 import com.geraldsaccount.neuefische_todo.model.openai.OpenAiResponse;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"servers.openai.url=http://localhost:8089"
+})
 @AutoConfigureMockMvc
+@WireMockTest(httpPort = 8089)
 public class ChatGptServiceTest {
 
 	@Value("${servers.openai.uri}")
-	private String url;
+	private String uri;
 
 	@Autowired
 	private ObjectMapper mapper;
@@ -33,7 +35,7 @@ public class ChatGptServiceTest {
 	private ChatGptService service;
 
 	@Test
-	void getCorrectedText_returnsCorrection_withInput() throws JsonProcessingException {
+	void getCorrectedText_returnsCorrection_withInput() throws JsonProcessingException, OpenAiException {
 		String misspelledText = "i are not hungri. me olredi eat.";
 		String correctedText = "I am not hungry. I already ate.";
 		OpenAiResponse response = new OpenAiResponse(
@@ -43,7 +45,7 @@ public class ChatGptServiceTest {
 						"completed",
 						List.of(
 								new OpenAiContent("output_text", correctedText)))));
-		WireMock.stubFor(WireMock.post(url)
+		WireMock.stubFor(WireMock.post(uri)
 				.willReturn(WireMock.ok()
 						.withHeader("Content-Type", "application/json")
 						.withBody(mapper.writeValueAsString(response))));
